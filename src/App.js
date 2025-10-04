6cccef4abeffb022ec7c03b00efaf76e66ff57bf
Matin Valizade <matinvlz77@gmail.com>
2025-10-04 20:49:59 +0330

    fix(router): use HashRouter for Github Pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Services from "./pages/Services";
 import AboutUs from "./pages/AboutUs";
 import OurTeam from "./pages/OurTeam";
@@ -9,27 +9,24 @@ import Footer from "./components/Footer/Footer";
 import Login from "./pages/Login";
 import ScrollToTop from "./components/ScrollToTop";
 
-
-const App = ()=> {
+const App = () => {
   return (
-
-      <BrowserRouter>
-          <div className="overflow-x-hidden bg-white text-dark">
-                <ScrollToTop />
-              <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/services" element={<Services />} />
-                  <Route path="/about" element={<AboutUs />} />
-                  <Route path="/team" element={<OurTeam />} />
-                  <Route path="/contact" element={<ContactUs />} />
-                  <Route path="/login" element={<Login />} />
-              </Routes>
-              <Footer />
-          </div>
-      </BrowserRouter>
-
-
+    <HashRouter
+    >
+      <div className="overflow-x-hidden bg-white text-dark">
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/team" element={<OurTeam />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </div>
+    </HashRouter>
   );
-}
+};
 
 export default App;
